84266804dea4a6ca49502e881df2a5c5645fc50b
Laraib Kamal Khan <khan1999laraib@gmail.com>
2023-06-27 12:18:50 +0530

    first commit

---
+++ simple.js
@@ -0,0 +1,15 @@
+const hex = ["red","green","blue","pink"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+   
+     const RandomNumber = 2;
+
+    
+
+  
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+  });
+  
